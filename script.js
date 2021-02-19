b9121b6d775832e0bdaeb09434ac6edec244e1b2
Sam <samrad84@yahoo.com>
2021-02-19 10:36:52 +0100

    Add Final Comments!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function searchShow(query) {
   let client_id = "KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS";
-  let client_secret = "D310XYOWDUFDSJKKZMP0WZSHLZXFDVR3VP5E2DDYUYSEVDGB";
+  let client_secret = "3GU4Y12IHV3SDB0CFAMTNGS4QEPBSWN3Q0IG2URPWWCOASE1";
   const api_key = "a7ae1818efc8758ead2b2a5a64eab221";
   const ApiUrl = "https://api.foursquare.com/v2/venues/";
 
@@ -36,25 +36,26 @@ function searchShow(query) {
     client_secret +
     "&v=20180323&limit=1&ll=40.7243,-74.0018&near=" +
     query;
-
-  fetch(weatherUrl)
-    .then((response) => response.json())
-    .then((jsonData) => {
-      results1 = jsonData.weather;
-      console.log("this is the jsonData.weather=", results1);
-    });
-
+ // Här hämtar jag data ur foursquare Api, med hjälp av Query och input value urdragen 
+ //search input. En array innehåller stadens sevärdigheter.
   fetch(SearchCityUrl)
     .then((response) => response.json())
     .then((jsonData) => {
       results = jsonData.response.venues;
-      console.log("this is the jsonData.response.venues=", results);
     });
+
+    //Här hämtar jag väder prognos från openweather api (Typ av väder).
+  fetch(weatherUrl)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      results1 = jsonData.weather;
+    });
+
+
   fetch(weatherUrl)
     .then((response) => response.json())
     .then((jsonData) => {
       results3 = jsonData.main;
-      console.log("this is the jsonData.main=", results3);
     });
 
   displayWeatherResults(results1);
@@ -62,23 +63,25 @@ function searchShow(query) {
   displayThetemp(results3);
 }
 
+// I dem funktionerna nedan bryter jag ner arrayen urdragen via fetch från apier
+// och via DOM visualiserar datan från json filen på element till html.
 function displayWeatherResults(results1) {
   results1.forEach((result1) => {
     console.log(result1);
 
     let parent = document.querySelector("main");
-    //var child = document.createElement("div");
-  
     var child = document.createElement("h4");
 
-  
-    child.innerText = result1.description;
+    var title = document.createElement("h3");
 
-    //child.className = "card";
-    //child.append(land);
+    child.innerText = result1.description;
+    title.innerText = result1.main;
+
     parent.append(child);
+    parent.append(title);
   });
 }
+
 function displayPlaceResults(results) {
   results.forEach((result) => {
     console.log("result=", result);
@@ -89,24 +92,23 @@ function displayPlaceResults(results) {
     var pa = document.createElement("p");
     var city = document.createElement("h1");
 
-
-parent.id = "main";
+    parent.id = "main";
     child.className = "card";
+
     let venuesId = result.id;
     const newUrl =
       "https://api.foursquare.com/v2/venues/" +
       venuesId +
-      "/photos?&client_id=KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS&client_secret=D310XYOWDUFDSJKKZMP0WZSHLZXFDVR3VP5E2DDYUYSEVDGB&v=20201002";
-    console.log("this is new Url =", newUrl);
-    const imgsrc = getImageUrl(newUrl);
+      "/photos?&client_id=KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS&client_secret=3GU4Y12IHV3SDB0CFAMTNGS4QEPBSWN3Q0IG2URPWWCOASE1&v=20201002";
+    let src = getImageUrl(newUrl);
+    imeg.src = src;
 
     city.innerText = result.location.city;
     title.innerText = result.name;
     pa.innerText = result.location.formattedAddress;
-    imeg.src = imgsrc;
-  
-    child.append(city);
+
     child.append(imeg);
+    child.append(city);
     child.append(title);
     child.append(pa);
     parent.append(child);
@@ -115,17 +117,14 @@ parent.id = "main";
 
 function displayThetemp(results3) {
   results3.forEach((result3) => {
-    console.log(result3);
-
     let parent = document.querySelector("main");
     var child = document.createElement("div");
-  
+
     var tempe = document.createElement("h3");
     var temp = result3.temp;
     let tempa = FarenhitetoC(temp);
     tempe.innerText = tempa;
 
- 
     child.className = "card";
     child.append(tempe);
     parent.append(child);
@@ -142,8 +141,7 @@ function getVenuesId(url) {
         const newUrl =
           "https://api.foursquare.com/v2/venues/" +
           venuesId +
-          "/photos?&client_id=KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS&client_secret=D310XYOWDUFDSJKKZMP0WZSHLZXFDVR3VP5E2DDYUYSEVDGB&v=20201002";
-        console.log("this is new Url =", newUrl);
+          "/photos?&client_id=KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS&client_secret=3GU4Y12IHV3SDB0CFAMTNGS4QEPBSWN3Q0IG2URPWWCOASE1&v=20201002";
       });
     });
 }
@@ -158,7 +156,6 @@ function getImageUrl(newUrl) {
         let suffixId = result1.suffix;
 
         const imageUrl = prefixId + "300x500/" + suffixId;
-        console.log("this is image Url =", imageUrl);
       });
     });
 }
@@ -170,7 +167,7 @@ function FarenhitetoC(tempe) {
 
 window.onload = () => {
   const SearchTermElement = document.getElementById("SearchTerm");
-  SearchTermElement.onkeypress = (event) => {
+  SearchTermElement.onkeyup = (event) => {
     searchShow(SearchTermElement.value);
   };
 };
