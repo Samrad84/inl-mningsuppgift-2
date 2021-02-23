21f3f41a63540a66194b0239385623a09abcd49f
Alireza Mohammadi <Samrad84@yahoo.com>
2021-02-23 16:07:08 +0100

    add checkbox

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
+
 function searchShow(query) {
   let client_id = "KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS";
   let client_secret = "3GU4Y12IHV3SDB0CFAMTNGS4QEPBSWN3Q0IG2URPWWCOASE1";
   const api_key = "a7ae1818efc8758ead2b2a5a64eab221";
   const ApiUrl = "https://api.foursquare.com/v2/venues/";
+ const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + api_key;
+ const SearchCityUrl = ApiUrl +"search?near=" +query + "&radius=10&client_id=" + client_id +"&client_secret=" + client_secret + "&v=20201002";
 
-  const weatherUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query +
-    "&appid=" +
-    api_key;
-
-  const photoUrl =
-    ApiUrl +
-    "51eabef6498e10cf3aea7942/photos?&client_id=" +
-    client_id +
-    "&client_secret=" +
-    client_secret +
-    "&v=20201002";
-
-  const SearchCityUrl =
-    ApiUrl +
-    "search?near=" +
-    query +
-    "&radius=10&client_id=" +
-    client_id +
-    "&client_secret=" +
-    client_secret +
-    "&v=20201002";
-
-  const ThingsUrl =
-    ApiUrl +
-    "explore?client_id=" +
-    client_id +
-    "&client_secret=" +
-    client_secret +
-    "&v=20180323&limit=1&ll=40.7243,-74.0018&near=" +
-    query;
   // Här hämtar jag data ur foursquare Api, med hjälp av Query och input value urdragen
   //search input. En array innehåller stadens sevärdigheter.
   fetch(SearchCityUrl)
     .then((response) => response.json())
     .then((jsonData) => {
       results = jsonData.response.venues;
+      displayPlaceResults(results);
     });
 
   //Här hämtar jag väder prognos från openweather api (Typ av väder).
@@ -49,6 +21,7 @@ function searchShow(query) {
     .then((response) => response.json())
     .then((jsonData) => {
       results1 = jsonData.weather;
+      displayWeatherResults(results1);
     });
 
   fetch(weatherUrl)
@@ -57,45 +30,48 @@ function searchShow(query) {
       results3 = jsonData.main;
       let tempa = results3.humidity;
       let parent = document.getElementById("weather");
-      let tempPlace=  document.createElement("h4");
-     tempPlace.innerText = FarenhitetoC(tempa);
-     parent.append(tempPlace);
-
+      let child = document.getElementById("div")
+      let tempPlace = document.createElement("h4");
+      tempPlace.innerText = FarenhitetoC(tempa);
+      child.append(tempPlace);
+      parent.append(child);
+      displayWeatherResults(result3);
     });
 
-  displayWeatherResults(results1);
-  displayPlaceResults(results);
-  //displayThetemp(results3);
-  displayWeatherResults(result3);
+
 }
 
 // I dem funktionerna nedan bryter jag ner arrayen urdragen via fetch från apier
 // och via DOM visualiserar datan från json filen på element till html.
+
 function displayWeatherResults(results1) {
   results1.forEach((result1) => {
-    console.log(result1);
+
 
     let parent = document.getElementById("weather");
     let child = document.createElement("div");
     let title = document.createElement("h3");
     let descr = document.createElement("h2");
 
-
-
     descr.innerText = result1.description;
     title.innerText = result1.main;
+child.id = "div";
+    const input1 = document.getElementById("input1");
 
+ if(input1.checked){
+  child.append(descr);
+  child.append(title);
+  parent.append(child);
+  
+  }
 
     
-    child.append(descr);
-    child.append(title);
-    parent.append(child);
   });
 }
 
 function displayPlaceResults(results) {
   results.forEach((result) => {
-    console.log("result=", result);
+ 
     let parent = document.querySelector("main");
     var child = document.createElement("div");
     var imeg = document.createElement("img");
@@ -106,30 +82,38 @@ function displayPlaceResults(results) {
     parent.id = "main";
     child.className = "card";
 
-    let venuesId = result.id;
+  
+    const categorie = result.categories;
+    categorie.forEach((categ) => {
+      city.innerText = categ.pluralName;
+      let venuesId = categ.id;
     const newUrl =
       "https://api.foursquare.com/v2/venues/" +
       venuesId +
       "/photos?&client_id=KB3VKVREN4S40G1J5O1MYZ2TGMCJZBMROPZWF4APWD5QX0CS&client_secret=3GU4Y12IHV3SDB0CFAMTNGS4QEPBSWN3Q0IG2URPWWCOASE1&v=20201002";
     let src = getImageUrl(newUrl);
     imeg.src = src;
-    const categorie = result.categories;
-    categorie.forEach((categ) => {
-      city.innerText = categ.pluralName;
     });
 
     title.innerText = result.name;
     pa.innerText = result.location.formattedAddress;
 
-    child.append(imeg);
+    const input2 = document.getElementById("input2");
+
+    if(input2.checked){
+
+      child.append(imeg);
     child.append(city);
     child.append(title);
     child.append(pa);
     parent.append(child);
+ 
+     
+     }
+
+    
   });
 }
-
-
 
 function getVenuesId(url) {
   fetch(url)
@@ -154,20 +138,20 @@ function getImageUrl(newUrl) {
       results1.forEach((result1) => {
         let prefixId = result1.prefix;
         let suffixId = result1.suffix;
-
         const imageUrl = prefixId + "300x500/" + suffixId;
+        return imageUrl;
       });
     });
 }
 
 function FarenhitetoC(tempe) {
-  var temp = (tempe - 32) / 5.9 ;
-  return Math.trunc(temp) ;
+  var temp = (tempe - 32) / 5.9;
+  return Math.trunc(temp);
 }
 
 window.onload = () => {
   const SearchTermElement = document.getElementById("SearchTerm");
-  SearchTermElement.onclick = (event) => {
+  SearchTermElement.onkeyup = (event) => {
     searchShow(SearchTermElement.value);
   };
 };
